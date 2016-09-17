6ef4b940f799836b4e813add367a3f2bb19c52ba
Lion <maolion.j@gmail.com>
2016-09-17 23:11:34 +0800

    fix

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,13 @@
 "use strict";
-var __extends = (this && this.__extends) || function (d, b) {
-    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
-    function __() { this.constructor = d; }
-    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-};
-var FS = require('fs-extra');
-var Path = require('path');
-var Chokidar = require('chokidar');
-var Events = require('events');
-var thenfail_1 = require('thenfail');
-var uid = 0;
-var TSConfigFilesSynchronizer = (function (_super) {
-    __extends(TSConfigFilesSynchronizer, _super);
-    function TSConfigFilesSynchronizer(targetFilePath, options) {
-        _super.call(this);
+const FS = require('fs-extra');
+const Path = require('path');
+const Chokidar = require('chokidar');
+const Events = require('events');
+const thenfail_1 = require('thenfail');
+let uid = 0;
+class TSConfigFilesSynchronizer extends Events.EventEmitter {
+    constructor(targetFilePath, options) {
+        super();
         options = options || {};
         this._targetFilePath = Path.resolve(targetFilePath);
         this._projectDir = options.projectDir || Path.dirname(this._targetFilePath);
@@ -24,40 +18,31 @@ var TSConfigFilesSynchronizer = (function (_super) {
         this._readyPromise = new thenfail_1.Promise();
         this._options = options;
         if (!FS.existsSync(this._targetFilePath)) {
-            throw new Error(this._targetFilePath + " is not found");
+            throw new Error(`${this._targetFilePath} is not found`);
         }
         this._watch();
     }
-    Object.defineProperty(TSConfigFilesSynchronizer.prototype, "files", {
-        get: function () {
-            return (this._tsconfig || {}).files || [];
-        },
-        set: function (files) {
-            if (!this._tsconfig) {
-                this._tsconfig = {};
-            }
-            this._tsconfig.files = files || [];
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(TSConfigFilesSynchronizer.prototype, "ready", {
-        get: function () {
-            return this._readyPromise;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    TSConfigFilesSynchronizer.prototype.sync = function (callback, delay) {
-        var _this = this;
+    set files(files) {
+        if (!this._tsconfig) {
+            this._tsconfig = {};
+        }
+        this._tsconfig.files = files || [];
+    }
+    get files() {
+        return (this._tsconfig || {}).files || [];
+    }
+    get ready() {
+        return this._readyPromise;
+    }
+    sync(callback, delay) {
         clearTimeout(this._syncDelayTimerId);
         if (Object.keys(this._tsconfig).join('') == 'files') {
             return thenfail_1.Promise.void;
         }
-        var promise = new thenfail_1.Promise();
+        const promise = new thenfail_1.Promise();
         if (delay) {
-            this._syncDelayTimerId = setTimeout(function () {
-                _this.sync(callback)
+            this._syncDelayTimerId = setTimeout(() => {
+                this.sync(callback)
                     .handle(promise);
             }, delay);
         }
@@ -67,94 +52,92 @@ var TSConfigFilesSynchronizer = (function (_super) {
                 return a > b ? 1 : -1;
             });
             this.emit('syncBefore');
-            var tsconfig_1 = this._tsconfig;
-            this._syncLocker.queue(function () {
-                return thenfail_1.Promise.invoke(FS.writeFile, _this._targetFilePath, JSON.stringify(tsconfig_1, null, 4))
-                    .then(function () {
+            let tsconfig = this._tsconfig;
+            this._syncLocker.queue(() => {
+                return thenfail_1.Promise.invoke(FS.writeFile, this._targetFilePath, JSON.stringify(tsconfig, null, 4))
+                    .then(() => {
                     callback && callback();
-                    _this.emit('sync');
-                    if (_this._readyPromise.pending) {
-                        _this._readyPromise.resolve();
+                    this.emit('sync');
+                    if (this._readyPromise.pending) {
+                        this._readyPromise.resolve();
                     }
                     promise.resolve();
                 })
-                    .fail(function (reason) {
-                    _this.emit('syncError', reason);
-                    if (_this._readyPromise.pending) {
-                        _this._readyPromise.reject(reason);
+                    .fail(reason => {
+                    this.emit('syncError', reason);
+                    if (this._readyPromise.pending) {
+                        this._readyPromise.reject(reason);
                     }
                     promise.reject(reason);
                 });
             });
         }
         return promise;
-    };
-    TSConfigFilesSynchronizer.prototype.destroy = function () {
+    }
+    destroy() {
         this._closeWatcher();
         this._tsconfig = null;
         this._files = null;
         this._watcher = null;
-    };
-    TSConfigFilesSynchronizer.prototype._closeWatcher = function () {
+    }
+    _closeWatcher() {
         try {
             if (this._watcher) {
                 this._watcher.close();
             }
         }
         catch (e) { }
-    };
-    TSConfigFilesSynchronizer.prototype._watch = function () {
-        var _this = this;
-        var id = ++uid;
-        var isReady = false;
+    }
+    _watch() {
+        let id = ++uid;
+        let isReady = false;
         this.files = this._options.files || [];
         this._closeWatcher();
         this._watcher = Chokidar.watch(uniqueArrayItems(this._fileGlobs.concat('tsconfig.json', this._extraFileGlobs)), { cwd: this._projectDir });
-        this._watcher.on("add", function (file) {
+        this._watcher.on("add", (file) => {
             if (id != uid) {
                 return;
             }
-            if (!_this._handleChange(file)) {
-                _this.files.push(file);
-                _this.sync(null, 100);
+            if (!this._handleChange(file)) {
+                this.files.push(file);
+                this.sync(null, 100);
             }
-            _this.emit('action', { type: 'add', file: file });
+            this.emit('action', { type: 'add', file: file });
         });
-        this._watcher.on("change", function (file) {
+        this._watcher.on("change", (file) => {
             if (id != uid) {
                 return;
             }
-            _this.emit('action', { type: 'change', file: file });
-            _this._handleChange(file);
+            this.emit('action', { type: 'change', file: file });
+            this._handleChange(file);
         });
-        this._watcher.on("unlink", function (file) {
+        this._watcher.on("unlink", (file) => {
             if (id != uid) {
                 return;
             }
-            var n = 0;
-            var files = _this.files;
-            for (var i = 0, l = files.length; i < l; i++) {
+            let n = 0;
+            let files = this.files;
+            for (let i = 0, l = files.length; i < l; i++) {
                 if (files[i] != file) {
                     files[n++] = files[i];
                 }
             }
             files.length = n;
-            _this.files = files;
-            _this.sync(null, 100);
-            _this.emit('action', { type: 'unlink', file: file });
+            this.files = files;
+            this.sync(null, 100);
+            this.emit('action', { type: 'unlink', file: file });
         });
-    };
-    TSConfigFilesSynchronizer.prototype._handleChange = function (file) {
+    }
+    _handleChange(file) {
         switch (file) {
             case 'tsconfig.json':
                 this._handleTSConfigChanged();
                 return true;
         }
-    };
-    TSConfigFilesSynchronizer.prototype._handleTSConfigChanged = function () {
-        var _this = this;
-        var id = uid;
-        FS.readFile(this._targetFilePath, function (err, tsconfig) {
+    }
+    _handleTSConfigChanged() {
+        let id = uid;
+        FS.readFile(this._targetFilePath, (err, tsconfig) => {
             if (err || id != uid) {
                 return;
             }
@@ -164,32 +147,31 @@ var TSConfigFilesSynchronizer = (function (_super) {
             catch (e) {
                 return;
             }
-            var newFileGlobs = tsconfig.fileGlobs || [];
-            var oldFileGlobs = _this._fileGlobs;
-            for (var i = 0, globs = oldFileGlobs.length > newFileGlobs.length ? oldFileGlobs : newFileGlobs, l = globs.length; i < l; i++) {
-                var glob = globs[i];
+            let newFileGlobs = tsconfig.fileGlobs || [];
+            let oldFileGlobs = this._fileGlobs;
+            for (let i = 0, globs = oldFileGlobs.length > newFileGlobs.length ? oldFileGlobs : newFileGlobs, l = globs.length; i < l; i++) {
+                let glob = globs[i];
                 if (newFileGlobs.indexOf(glob) > -1 && oldFileGlobs.indexOf(glob) > -1) {
                     continue;
                 }
                 else {
-                    _this._fileGlobs = newFileGlobs;
-                    _this._tsconfig = tsconfig;
-                    _this._watch();
+                    this._fileGlobs = newFileGlobs;
+                    this._tsconfig = tsconfig;
+                    this._watch();
                     return;
                 }
             }
         });
-    };
-    return TSConfigFilesSynchronizer;
-}(Events.EventEmitter));
+    }
+}
 exports.TSConfigFilesSynchronizer = TSConfigFilesSynchronizer;
-exports.__esModule = true;
-exports["default"] = TSConfigFilesSynchronizer;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = TSConfigFilesSynchronizer;
 function uniqueArrayItems(list) {
-    var map = {};
-    var n = 0;
-    for (var i = 0, l = list.length; i < l; i++) {
-        var item = list[i];
+    const map = {};
+    let n = 0;
+    for (let i = 0, l = list.length; i < l; i++) {
+        let item = list[i];
         if (!map[item]) {
             map[item] = true;
             list[n++] = item;
--- dist/test/tsconfig-files-synchronizer.js
+++ dist/test/tsconfig-files-synchronizer.js
@@ -1,46 +1,46 @@
 "use strict";
-var FS = require('fs-extra');
-var Path = require('path');
-var chai_1 = require('chai');
-var _1 = require('../');
-var TEST_PROJECT_DIR = Path.join(__dirname, '../../.cache/test-project-' + Date.now());
-var TEST_PROJECT_TSCONFIG_FILE = Path.join(TEST_PROJECT_DIR, "tsconfig.json");
-var FILES = [
+const FS = require('fs-extra');
+const Path = require('path');
+const chai_1 = require('chai');
+const _1 = require('../');
+const TEST_PROJECT_DIR = Path.join(__dirname, '../../.cache/test-project-' + Date.now());
+const TEST_PROJECT_TSCONFIG_FILE = Path.join(TEST_PROJECT_DIR, "tsconfig.json");
+const FILES = [
     "extra-file.ts",
     "file1.ts",
     "dir1/file1.ts",
 ].sort(sortFunc);
 initTestProject();
-describe("tsconfig-files-synchronizer", function () {
-    var synchronizer;
-    it("new TSConfigFilesSynchronizer(...)", function () {
-        synchronizer = new _1["default"](TEST_PROJECT_TSCONFIG_FILE, {
+describe("tsconfig-files-synchronizer", () => {
+    let synchronizer;
+    it("new TSConfigFilesSynchronizer(...)", () => {
+        synchronizer = new _1.default(TEST_PROJECT_TSCONFIG_FILE, {
             files: [
                 "extra-file.ts"
             ]
         });
         return true;
     });
-    it("TSConfigFilesSynchronizer.ready", function (done) {
+    it("TSConfigFilesSynchronizer.ready", (done) => {
         synchronizer.ready
             .then(done)
-            .fail(function (reason) {
+            .fail(reason => {
             chai_1.expect(reason, reason).to.be.equal('');
             done();
         });
     });
-    it("检查files是否同步正常", function () {
+    it("检查files是否同步正常", () => {
         compareList(getFiles(), FILES);
         return true;
     });
-    it("检查新目标文件添加", function (done) {
-        var count = 0;
-        synchronizer.on('action', function (action) {
+    it("检查新目标文件添加", (done) => {
+        let count = 0;
+        synchronizer.on('action', (action) => {
             if (action && action.type == 'add') {
                 count++;
             }
         });
-        synchronizer.once('sync', function () {
+        synchronizer.once('sync', () => {
             chai_1.expect(count).to.be.equal(2);
             compareList(getFiles(), FILES
                 .concat(['file2.ts', 'dir1/file2.ts'])
@@ -51,20 +51,20 @@ describe("tsconfig-files-synchronizer", function () {
         FS.ensureFileSync(Path.join(TEST_PROJECT_DIR, 'dir1/file2.ts'));
         FS.ensureFileSync(Path.join(TEST_PROJECT_DIR, 'dir1/file3.tsx'));
     });
-    it("目标tsconfig.json文件被修改", function (done) {
-        var count = 0;
-        synchronizer.once('sync', function () {
+    it("目标tsconfig.json文件被修改", (done) => {
+        let count = 0;
+        synchronizer.once('sync', () => {
             compareList(getFiles(), FILES
                 .concat(['file2.ts', 'dir1/file2.ts', 'dir1/file3.tsx'])
                 .sort(sortFunc));
             done();
         });
-        var tsconfig = FS.readJSONSync(TEST_PROJECT_TSCONFIG_FILE);
+        const tsconfig = FS.readJSONSync(TEST_PROJECT_TSCONFIG_FILE);
         tsconfig.fileGlobs.push('./**/*.tsx');
         FS.writeFileSync(TEST_PROJECT_TSCONFIG_FILE, JSON.stringify(tsconfig, null, 4));
     });
-    it("添加新目录且包含匹配文件", function (done) {
-        synchronizer.once('sync', function () {
+    it("添加新目录且包含匹配文件", (done) => {
+        synchronizer.once('sync', () => {
             compareList(getFiles(), FILES
                 .concat(['file2.ts', 'dir2/file1.ts', 'dir1/file2.ts', 'dir1/file3.tsx'])
                 .sort(sortFunc));
@@ -72,9 +72,9 @@ describe("tsconfig-files-synchronizer", function () {
         });
         FS.ensureFileSync(Path.join(TEST_PROJECT_DIR, 'dir2/file1.ts'));
     });
-    it("删除匹配文件和删除目录", function (done) {
-        var count = 0;
-        synchronizer.once('sync', function () {
+    it("删除匹配文件和删除目录", (done) => {
+        let count = 0;
+        synchronizer.once('sync', () => {
             compareList(getFiles(), FILES
                 .concat(['file2.ts', 'dir1/file3.tsx'])
                 .sort(sortFunc));
@@ -83,7 +83,7 @@ describe("tsconfig-files-synchronizer", function () {
         FS.unlinkSync(Path.join(TEST_PROJECT_DIR, 'dir1/file2.ts'));
         FS.remove(Path.join(TEST_PROJECT_DIR, 'dir2'));
     });
-    it("销毁", function () {
+    it("销毁", () => {
         synchronizer.destroy();
         try {
             FS.removeSync(TEST_PROJECT_DIR);
@@ -100,12 +100,31 @@ function sortFunc(a, b) {
 function initTestProject() {
     FS.ensureDirSync(TEST_PROJECT_DIR);
     FS.emptyDirSync(TEST_PROJECT_DIR);
-    FS.writeFileSync(Path.join(TEST_PROJECT_DIR, 'tsconfig.json'), "\n        {\n            \"compilerOptions\": {\n                \"target\": \"es6\",\n                \"module\": \"commonjs\",\n                \"declaration\": false,\n                \"noImplicitAny\": true,\n                \"removeComments\": true,\n                \"noEmitOnError\": false,\n                \"preserveConstEnums\": true,\n                \"noLib\": false,\n                \"sourceMap\": false,\n                \"experimentalDecorators\": true\n            },\n            \"fileGlobs\": [\n                \"./**/*.ts\"\n            ],\n            \"files\": []\n        }\n        ");
+    FS.writeFileSync(Path.join(TEST_PROJECT_DIR, 'tsconfig.json'), `
+        {
+            "compilerOptions": {
+                "target": "es6",
+                "module": "commonjs",
+                "declaration": false,
+                "noImplicitAny": true,
+                "removeComments": true,
+                "noEmitOnError": false,
+                "preserveConstEnums": true,
+                "noLib": false,
+                "sourceMap": false,
+                "experimentalDecorators": true
+            },
+            "fileGlobs": [
+                "./**/*.ts"
+            ],
+            "files": []
+        }
+        `);
     FS.ensureFileSync(Path.join(TEST_PROJECT_DIR, 'file1.ts'));
     FS.ensureFileSync(Path.join(TEST_PROJECT_DIR, 'dir1/file1.ts'));
 }
 function getFiles() {
-    var tsconfig = FS.readJSONSync(TEST_PROJECT_TSCONFIG_FILE);
+    const tsconfig = FS.readJSONSync(TEST_PROJECT_TSCONFIG_FILE);
     return tsconfig.files || [];
 }
 function compareList(listA, listB, message) {
